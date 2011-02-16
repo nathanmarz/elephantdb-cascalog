46f96dad67767ae04e93c01c8aab61f28240a405
Nathan Marz <nathan.marz@gmail.com>
2011-02-15 19:12:59 -0800

    new project.clj

---
+++ project.clj
@@ -0,0 +1,17 @@
+(defproject elephantdb/elephantdb-cascalog "0.0.5"
+  :source-path "src/clj"
+  :test-path "test/clj"
+  :java-source-path "src/jvm"
+  :java-fork "true"
+  :javac-debug "true"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [elephantdb/elephantdb-cascading "0.0.4"]
+                 [cascalog "1.6.0"]
+                 ]
+  :dev-dependencies [
+                     [org.apache.hadoop/hadoop-core "0.20.2-dev"]
+                     [swank-clojure "1.2.1"]
+                    ]
+  :namespaces :all
+)
