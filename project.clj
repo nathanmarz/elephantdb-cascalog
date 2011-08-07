14d8513b2fa9b13ff297556be88efe2f911973b2
Sam Ritchie <sritchie09@gmail.com>
2011-08-07 17:05:49 -0400

    Updated internal cascalog version to 1.8.0; because elephantdb-cascalog's AOT compiled, this code will now break against versions of cascalog earlier than 1.8.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject elephantdb/elephantdb-cascalog "0.0.5"
+(defproject elephantdb/elephantdb-cascalog "0.0.6"
   :source-path "src/clj"
   :test-path "test/clj"
   :java-source-path "src/jvm"
@@ -7,11 +7,11 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [elephantdb/elephantdb-cascading "0.0.4"]
-                 [cascalog "1.6.0"]
+                 [cascalog "1.8.0"]
                  ]
   :dev-dependencies [
                      [org.apache.hadoop/hadoop-core "0.20.2-dev"]
                      [swank-clojure "1.2.1"]
-                    ]
-  :namespaces :all
-)
+                     ]
+  :aot :all
+  )
