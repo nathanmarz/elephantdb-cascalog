d6de3f705279280f5fd17e65a533b7a83fdecd79
Sam Ritchie <sritchie@twitter.com>
2011-09-05 00:06:11 -0700

    bumped project dependencies and version number.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject elephantdb/elephantdb-cascalog "0.0.6"
+(defproject elephantdb/elephantdb-cascalog "0.0.7"
   :source-path "src/clj"
   :test-path "test/clj"
   :java-source-path "src/jvm"
@@ -6,11 +6,7 @@
   :javac-debug "true"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [elephantdb/elephantdb-cascading "0.0.4"]
-                 [cascalog "1.8.0"]
-                 ]
-  :dev-dependencies [
-                     [org.apache.hadoop/hadoop-core "0.20.2-dev"]
-                     [swank-clojure "1.2.1"]
-                     ]
-  )
+                 [elephantdb/elephantdb-cascading "0.0.7"]
+                 [cascalog "1.8.0"]]
+  :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
+                     [swank-clojure "1.2.1"]])
